9de5035ec90b380fe476169af39c39bb14677b4b
Victor Pozdeev <victorpozdeev@gmail.com>
2022-11-24 15:30:32 +0400

    Фиксит баг с недекодированным URL в контроллере

--- Api/Controllers/DictionaryContentElementsController.cs
+++ Api/Controllers/DictionaryContentElementsController.cs
@@ -9,19 +9,20 @@ using System.Security.Claims;
 namespace FiveWords.Api.Controllers;
 
 [ApiController]
-[Route("[controller]/{dictionaryName}")]
+[Route("[controller]/{dictionaryNameEscaped}")]
 //[Produces("application/json")]
 public class DictionaryContentElementsController : ControllerBase
 {
     [HttpPut("{id}")]
     [Authorize]
-    public IActionResult UpdateWordTranslation(string dictionaryName, string id, [FromBody] WordTranslation newValue, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
+    public IActionResult UpdateWordTranslation(string dictionaryNameEscaped, string id, [FromBody] WordTranslation newValue, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
     {
         //if (!ModelState.IsValid)
         //    return ValidationProblem();
 
         var currentUser = usersRepository.Get(User.Identity!.Name!);
         var userDictionariesRepo = userDictionariesRepoManager.GetRepository(currentUser!);
+        var dictionaryName = Uri.UnescapeDataString(dictionaryNameEscaped);
         userDictionariesRepo.TryUpdateContentElementAndImmediatelySave(dictionaryName, id, newValue, out ActionError actionError);
         if (actionError is not null)
             return UnprocessableEntity(new { Error = actionError });
@@ -30,10 +31,11 @@ public class DictionaryContentElementsController : ControllerBase
 
     [HttpPost]
     [Authorize]
-    public IActionResult AddWordTranslation(string dictionaryName, [FromBody] WordTranslation valueToAdd, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
+    public IActionResult AddWordTranslation(string dictionaryNameEscaped, [FromBody] WordTranslation valueToAdd, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
     {
         var currentUser = usersRepository.Get(User.Identity!.Name!);
         var userDictionariesRepo = userDictionariesRepoManager.GetRepository(currentUser!);
+        var dictionaryName = Uri.UnescapeDataString(dictionaryNameEscaped);
         userDictionariesRepo.TryAddContentElementAndImmediatelySave(dictionaryName, valueToAdd, out ActionError actionError);
         if (actionError is not null)
             return Conflict(new { Error = actionError });
@@ -42,10 +44,11 @@ public class DictionaryContentElementsController : ControllerBase
 
     [HttpDelete("{id}")]
     [Authorize]
-    public IActionResult DeleteWordTranslation(string dictionaryName, string id, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
+    public IActionResult DeleteWordTranslation(string dictionaryNameEscaped, string id, [FromServices] IUsersRepository usersRepository, [FromServices] UserDictionariesUserRepositoriesManager userDictionariesRepoManager)
     {
         var currentUser = usersRepository.Get(User.Identity!.Name!);
         var userDictionariesRepo = userDictionariesRepoManager.GetRepository(currentUser!);
+        var dictionaryName = Uri.UnescapeDataString(dictionaryNameEscaped);
         userDictionariesRepo.TryDeleteContentElementAndImmediatelySave(dictionaryName, id, out ActionError actionError);
         if (actionError is not null)
             return UnprocessableEntity(new { Error = actionError });
