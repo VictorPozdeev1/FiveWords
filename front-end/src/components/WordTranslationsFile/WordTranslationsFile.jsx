265dfd192977e10a500cb7dea4091c930c4c6a39
Victor Pozdeev <victorpozdeev@gmail.com>
2022-11-29 23:56:17 +0400

    Добавляет загрузку файла со словами (*работает, но не закончено!)

--- front-end/src/components/WordTranslationsFile/WordTranslationsFile.jsx
+++ front-end/src/components/WordTranslationsFile/WordTranslationsFile.jsx
@@ -1,5 +1,6 @@
 ﻿import { useCallback } from 'react';
 import styles from './WordTranslationsFile.module';
+import { fetchWithAuth } from '../../modules/Auth.js';
 
 const WordTranslationsFile = () => {
     const saveSampleFile = useCallback(() => {
@@ -9,10 +10,34 @@ const WordTranslationsFile = () => {
         link.click();
     }, []);
 
+    const uploadFile = useCallback(() => {
+        const fileInput = document.createElement('input');
+        fileInput.setAttribute('type', 'file');
+        fileInput.addEventListener("change", () => { handleFiles(fileInput.files) }, false);
+        fileInput.click();
+    }, []);
+
+    const handleFiles = async files => {
+        if (files && files.length && files[0].size < 50000) {
+            const file = files[0];
+            console.log('size:', file.size);
+            const formData = new FormData();
+            formData.append('uploadingFile', file, 'words-to-add.csv');
+            const url = encodeURI('/DictionaryContentFile/Test1');
+            const options = {
+                method: 'POST',
+                body: formData
+            }
+            const response = await fetchWithAuth(url, options);
+            const json = await response.json();
+            console.log(json);
+        }
+    }
+
     return (
         <div>
             <button
-                //onClick={() => { setElementCreatorIsActive(true) }}
+                onClick={uploadFile}
                 className={styles.addFromFileButton}
             >
                 Добавить слова из файла...
