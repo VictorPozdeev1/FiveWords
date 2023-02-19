7f43a6cc7670eec2205d782fed857c94e596118c
Victor Pozdeev <victorpozdeev@gmail.com>
2023-02-19 07:22:27 +0400

    Удаляет FiveWords.v1

--- FiveWords.v1/View/ChallengeResult_HtmlViewCreator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FiveWords._v1.View;
-
-public abstract class ChallengeResult_HtmlViewCreator<TChallengeResult> : HtmlViewCreator<TChallengeResult>
-{
-    protected override string HtmlTemplateFilename => "challenge-result.html";
-}
