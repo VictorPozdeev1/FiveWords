c5784dd5c6041a17ffcb164faa06422e0e64ba98
Victor Pozdeev <victorpozdeev@gmail.com>
2023-02-19 16:52:13 +0400

    Настраивает подключение к БД

---
+++ FiveWords.Overall/Repository/EFRepository/CommonDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FiveWords.DataObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiveWords.Overall.Repository.EFRepository;
+
+public class CommonDbContext : DbContext
+{
+    public CommonDbContext(DbContextOptions<CommonDbContext> options)
+        : base(options)
+        => Database.EnsureCreated();
+
+    public DbSet<User> Users => Set<User>();
+}
