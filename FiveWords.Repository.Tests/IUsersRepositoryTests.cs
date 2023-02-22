8800066788cebe9dff126a81b6e1a50b160a5be8
Victor Pozdeev <victorpozdeev@gmail.com>
2023-02-22 12:06:34 +0400

    Добавляет первые тесты на UsersRepository

---
+++ FiveWords.Repository.Tests/IUsersRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using FiveWords.Repository.Interfaces;
+using FiveWords.DataObjects;
+using FiveWords.Repository.Tests;
+
+[TestFixture(typeof(SimpleRepositoryHelper))]
+internal class IUsersRepository_Get_Tests<TRepositoryHelper>
+    where TRepositoryHelper : ISimpleRepositoryHelper, new()
+{
+    TRepositoryHelper repositoryHelper;
+    IUsersRepository? systemUnderTests;
+
+    [OneTimeSetUp]
+    public void InitializeHelper() => repositoryHelper = new TRepositoryHelper();
+
+    /*[SetUp]
+    public void SetUp() => systemUnderTests = repositoryHelper.CreateRepository();*/
+
+    [TearDown]
+    public void TearDown() => repositoryHelper.DeleteRepository();
+
+    [OneTimeTearDown]
+    public void Clean() => repositoryHelper.Clean();
+
+    [Test]
+    [TestCase("Vasya Petrov", "6F9619FF-8B86-D011-B42D-00CF4FC964FF")]
+    [TestCase("Misha Hrenov", "6F9619FF-8B86-D011-B42D-00CF4FC964FF")]
+    public void IfSingleSuchUserExists_ThenReturnsIt(string id, string guidString)
+    {
+        systemUnderTests = repositoryHelper.CreateRepositoryWithOneEntity(id, guidString);
+        User expected = new User()
+        {
+            Id = id,
+            Guid = Guid.Parse(guidString)
+        };
+
+        User? actual = systemUnderTests.Get(id);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    public void IfNoSuchUserExists_ThenReturnsNull()
+    {
+        Assert.Pass();
+    }
+
+    // Такая ситуация в принципе не должна быть допущена любой реализацией интерфейса
+    //public void IfMultipleSuchUsersExist_ThenWhatToDo()
+    //{
+    //    Assert.Fail();
+    //}
+}
+
+public class IUsersRepository_GetAll_Tests
+{
+
+}
---
+++ FiveWords.Repository.Tests/SimpleRepositoryBuilder.cs
@@ -0,0 +1,80 @@
+﻿using FiveWords.DataObjects;
+using FiveWords.Repository.CsvRepository;
+using FiveWords.Repository.Interfaces;
+
+namespace FiveWords.Repository.Tests;
+
+internal interface ISimpleRepositoryHelper
+{
+    UsersCsvRepository CreateRepository();
+    UsersCsvRepository CreateRepositoryWithOneEntity(string id, string guidString);
+    void DeleteRepository();
+    void Clean();
+}
+
+internal enum RepositoryHelperState
+{
+    NotInitialized,
+    RepositoryNotCreated,
+    RepositoryCreated,
+    Cleaned
+}
+
+internal sealed class SimpleRepositoryHelper : ISimpleRepositoryHelper
+{
+    public SimpleRepositoryHelper()
+    {
+        filesId = Guid.NewGuid().ToString(); //check - is it not initialized?
+        Directory.CreateDirectory(filesId);
+        state = RepositoryHelperState.RepositoryNotCreated;
+    }
+
+    RepositoryHelperState state;
+
+    string filesId;
+
+    public UsersCsvRepository CreateRepository()
+    {
+        if (state != RepositoryHelperState.RepositoryNotCreated)
+            throw new InvalidOperationException();
+        var result = new UsersCsvRepository(filesId, $"{filesId}.csv");
+        state = RepositoryHelperState.RepositoryCreated;
+        return result;
+    }
+
+    public UsersCsvRepository CreateRepositoryWithOneEntity(string id, string guidString)
+    {
+        if (state != RepositoryHelperState.RepositoryNotCreated)
+            throw new InvalidOperationException();
+        File.WriteAllText(Path.Combine(filesId, $"{filesId}.csv"), $"Id,Guid\r\n{id},{guidString}\r\n");
+        var result = new UsersCsvRepository(filesId, $"{filesId}.csv");
+        state = RepositoryHelperState.RepositoryCreated;
+        return result;
+    }
+
+    public void DeleteRepository()
+    {
+        if (state != RepositoryHelperState.RepositoryCreated)
+            throw new InvalidOperationException();
+        File.Delete(Path.Combine(filesId, $"{filesId}.csv"));
+        state = RepositoryHelperState.RepositoryNotCreated;
+    }
+
+    public void Clean()
+    {
+        Directory.Delete(filesId, true);
+        state = RepositoryHelperState.Cleaned;
+    }
+
+    // CreateRepoWithOneEntity() { } ??????
+
+    //public ISimpleEntityRepository BuildRepository()
+    //{
+    //    return null;
+    //}
+
+    protected SimpleRepositoryHelper Initialize()
+    {
+        return this;
+    }
+}
