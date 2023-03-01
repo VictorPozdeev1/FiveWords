e8e4b9c9ec0f7574a5e665e1c497c65773511c9d
Victor Pozdeev <victorpozdeev@gmail.com>
2023-03-01 08:21:57 +0400

    WIP

---
+++ FiveWords.Repository.Tests/Helpers/EF/UsersRepositoryHelper.cs
@@ -0,0 +1,37 @@
+﻿using FiveWords.DataObjects;
+using FiveWords.Repository.Interfaces;
+
+namespace FiveWords.Repository.Tests.Helpers.EF;
+
+internal sealed class UsersRepositoryHelper : ISimpleEntityRepositoryHelper<User, string>
+{
+    public void Clean()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ISimpleEntityRepository<User, string> CreateRepositoryWithOneEntity(User singleEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ISimpleEntityRepository<User, string> CreateRepositoryWithSomeEntities(IEnumerable<User> entitiesToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteRepository()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<User> GetAllEntitiesFromRepository()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetSimilarButNotExistingId(string exampleId)
+    {
+        throw new NotImplementedException();
+    }
+}
