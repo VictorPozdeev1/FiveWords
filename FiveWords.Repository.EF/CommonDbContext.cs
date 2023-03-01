7e1e5d93c8aa74c8d0e20f38e649d92c430a3f62
Victor Pozdeev <victorpozdeev@gmail.com>
2023-03-01 09:00:13 +0400

    WIP // add Repository.EF project

---
+++ FiveWords.Repository.EF/CommonDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FiveWords.DataObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiveWords.Repository.EF;
+
+public class CommonDbContext : DbContext
+{
+    public CommonDbContext(DbContextOptions<CommonDbContext> options)
+        : base(options)
+        => Database.EnsureCreated();
+
+    public DbSet<User> Users => Set<User>();
+}
---
+++ FiveWords.Repository.EF/UsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using FiveWords.Repository.Interfaces;
+
+namespace FiveWords.Repository.EF;
+
+public class UsersRepository : IBaseRepository
+{
+    public UsersRepository(CommonDbContext dbContext)
+        => this.dbContext = dbContext;
+    private readonly CommonDbContext dbContext;
+    public int Test => dbContext.Users.Count();
+}
