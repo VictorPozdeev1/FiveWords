f09e6e7d7ee73b502b3527e57e1e754553b30481
Victor Pozdeev <victorpozdeev@gmail.com>
2023-03-04 16:34:29 +0400

    WIP

--- FiveWords.Repository.EF/UsersRepository.cs
+++ FiveWords.Repository.EF/UsersRepository.cs
@@ -12,17 +12,11 @@ public class UsersRepository : ISimpleEntityRepository<User, string>
 
     public bool Exists(string id)
     {
-        // Отправляет запрос в бд или только локально ищет?
         return dbContext.Set<User>().Any(it => it.Id.Equals(id));
     }
 
     public User? Get(string id)
     {
-        /* ??????
-         * More importantly is the difference in behaviour when using EntityFramework, since .FirstOrDefault() executes a query and adds the result to the change tracker (by default; use .AsTracking()/.AsNoTracking() and the settings of the DBContext for override). The execution of .FirstOrDefault() might fail with an exception, when the object is already tracked by the change tracker.
-         * .Find() will return the object that is already tracked by the change tracker in memory (ie. after an update or add). So this will not cause a problem when the object is already tracked.
-         */
-
         return dbContext.Set<User>().FirstOrDefault(it => it.Id.Equals(id));
     }
 
@@ -33,26 +27,42 @@ public class UsersRepository : ISimpleEntityRepository<User, string>
 
     public void AddAndImmediatelySave(User entity)
     {
+        if (Exists(entity.Id))
+            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
         dbContext.Set<User>().Add(entity);
         dbContext.SaveChanges();
     }
 
     public void UpdateAndImmediatelySave(string id, User entity)
     {
-        //dbContext.Set<User>().Remove(id);
-        if (dbContext.Set<User>().Single(it => it.Id.Equals(id)) is User foundUser)
-        {
-            dbContext.Entry(foundUser).State = EntityState.Detached;
-            //DbContext.Set<User>().Attach(entity); // not needed?
-            dbContext.Entry(entity).State = EntityState.Modified;
-            dbContext.SaveChanges();
-        }
+        var foundUser = dbContext.Find<User>(id);
+        if (foundUser is null)
+            throw new ArgumentException($"Entity with key {id} not found.");
+        if (Exists(entity.Id))
+            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
+        dbContext.Entry(foundUser).State = EntityState.Deleted;
+        dbContext.Entry(entity).State = EntityState.Added;
+        dbContext.SaveChanges();
+
+        /*  if (dbContext.Set<User>().Single(it => it.Id.Equals(id)) is User foundUser)
+          {
+              //var t = await db.Teachers.FindAsync(id);
+              //teacher.Id = t.Id; // you might need to return to original value 
+              //db.Entry(t).CurrentValues.SetValues(teacher);
+
+              dbContext.Entry(foundUser).State = EntityState.Detached;
+              //DbContext.Set<User>().Attach(entity); // not needed?
+              dbContext.Entry(entity).State = EntityState.Modified;
+              dbContext.SaveChanges();
+          }*/
     }
 
     public void DeleteAndImmediatelySave(string id)
     {
-        // Find(), then Remove()?
-        dbContext.Entry(new User() { Id = id }).State = EntityState.Deleted;
+        var entityToDelete = dbContext.Find<User>(id);
+        if (entityToDelete is null)
+            throw new ArgumentException($"Entity with key {id} not found.");
+        dbContext.Entry(entityToDelete).State = EntityState.Deleted;
         dbContext.SaveChanges();
     }
 }
