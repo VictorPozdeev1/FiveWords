383ff275a00bf55e5c154461a010dc09cfc23e7f
Victor Pozdeev <victorpozdeev@gmail.com>
2023-03-05 16:35:10 +0400

    WIP

---
+++ FiveWords.Repository.EF/GenericSimpleEntityRepository.cs
@@ -0,0 +1,71 @@
+﻿using FiveWords.DataObjects;
+using FiveWords.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiveWords.Repository.EF;
+
+public class GenericSimpleEntityRepository<TEntity, TId> : ISimpleEntityRepository<TEntity, TId>
+    where TEntity : BaseEntity<TId>
+    where TId : IEquatable<TId>
+{
+    public GenericSimpleEntityRepository(CommonDbContext dbContext)
+        => this.dbContext = dbContext;
+    private readonly CommonDbContext dbContext;
+
+    public bool Exists(TId id)
+    {
+        return dbContext.Set<TEntity>().Any(it => it.Id.Equals(id));
+    }
+
+    public TEntity? Get(TId id)
+    {
+        return dbContext.Set<TEntity>().FirstOrDefault(it => it.Id.Equals(id));
+    }
+
+    public IReadOnlyDictionary<TId, TEntity> GetAll()
+    {
+        return dbContext.Set<TEntity>().ToDictionary(it => it.Id);
+    }
+
+    public void AddAndImmediatelySave(TEntity entity)
+    {
+        if (Exists(entity.Id))
+            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
+        dbContext.Set<TEntity>().Add(entity);
+        dbContext.SaveChanges();
+    }
+
+    public void UpdateAndImmediatelySave(TId id, TEntity entity)
+    {
+        var foundEntity = dbContext.Find<User>(id);
+        if (foundEntity is null)
+            throw new ArgumentException($"Entity with key {id} not found.");
+        if (Exists(entity.Id))
+            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
+        dbContext.Entry(foundEntity).State = EntityState.Deleted;
+        dbContext.Entry(entity).State = EntityState.Added;
+        dbContext.SaveChanges();
+
+        /*  if (dbContext.Set<User>().Single(it => it.Id.Equals(id)) is User foundUser)
+          {
+              //var t = await db.Teachers.FindAsync(id);
+              //teacher.Id = t.Id; // you might need to return to original value
+              //db.Entry(t).CurrentValues.SetValues(teacher);
+
+              dbContext.Entry(foundUser).State = EntityState.Detached;
+              //DbContext.Set<User>().Attach(entity); // not needed?
+              dbContext.Entry(entity).State = EntityState.Modified;
+              dbContext.SaveChanges();
+          }*/
+    }
+
+    public void DeleteAndImmediatelySave(TId id)
+    {
+        var entityToDelete = dbContext.Find<TEntity>(id);
+        if (entityToDelete is null)
+            throw new ArgumentException($"Entity with key {id} not found.");
+        dbContext.Entry(entityToDelete).State = EntityState.Deleted;
+        dbContext.SaveChanges();
+    }
+}
+
--- FiveWords.Repository.EF/UsersRepository.cs
+++ FiveWords.Repository.EF/UsersRepository.cs
@@ -4,65 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FiveWords.Repository.EF;
 
-public class UsersRepository : ISimpleEntityRepository<User, string>
+public class UsersRepository : GenericSimpleEntityRepository<User, string>
 {
-    public UsersRepository(CommonDbContext dbContext)
-        => this.dbContext = dbContext;
-    private readonly CommonDbContext dbContext;
-
-    public bool Exists(string id)
-    {
-        return dbContext.Set<User>().Any(it => it.Id.Equals(id));
-    }
-
-    public User? Get(string id)
-    {
-        return dbContext.Set<User>().FirstOrDefault(it => it.Id.Equals(id));
-    }
-
-    public IReadOnlyDictionary<string, User> GetAll()
-    {
-        return dbContext.Set<User>().ToDictionary(it => it.Id);
-    }
-
-    public void AddAndImmediatelySave(User entity)
-    {
-        if (Exists(entity.Id))
-            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
-        dbContext.Set<User>().Add(entity);
-        dbContext.SaveChanges();
-    }
-
-    public void UpdateAndImmediatelySave(string id, User entity)
-    {
-        var foundUser = dbContext.Find<User>(id);
-        if (foundUser is null)
-            throw new ArgumentException($"Entity with key {id} not found.");
-        if (Exists(entity.Id))
-            throw new ArgumentException($"Entity with key {entity.Id} already exists.");
-        dbContext.Entry(foundUser).State = EntityState.Deleted;
-        dbContext.Entry(entity).State = EntityState.Added;
-        dbContext.SaveChanges();
-
-        /*  if (dbContext.Set<User>().Single(it => it.Id.Equals(id)) is User foundUser)
-          {
-              //var t = await db.Teachers.FindAsync(id);
-              //teacher.Id = t.Id; // you might need to return to original value 
-              //db.Entry(t).CurrentValues.SetValues(teacher);
-
-              dbContext.Entry(foundUser).State = EntityState.Detached;
-              //DbContext.Set<User>().Attach(entity); // not needed?
-              dbContext.Entry(entity).State = EntityState.Modified;
-              dbContext.SaveChanges();
-          }*/
-    }
-
-    public void DeleteAndImmediatelySave(string id)
-    {
-        var entityToDelete = dbContext.Find<User>(id);
-        if (entityToDelete is null)
-            throw new ArgumentException($"Entity with key {id} not found.");
-        dbContext.Entry(entityToDelete).State = EntityState.Deleted;
-        dbContext.SaveChanges();
-    }
+    public UsersRepository(CommonDbContext dbContext) : base(dbContext) { }
 }
