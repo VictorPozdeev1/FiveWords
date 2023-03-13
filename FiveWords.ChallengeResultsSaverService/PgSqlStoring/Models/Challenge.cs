57ea5c87b86a32e7dbb7fa542d93d214b5f2dff9
Victor Pozdeev <victorpozdeev@gmail.com>
2023-03-13 15:24:13 +0400

    WIP

--- FiveWords.ChallengeResultsSaverService/PgSqlStoring/Models/Challenge.cs
+++ FiveWords.ChallengeResultsSaverService/PgSqlStoring/Models/Challenge.cs
@@ -5,12 +5,21 @@ namespace FiveWords.ChallengeResultsSaverService.PgSqlStoring.Models;
 [Table("challenges")]
 internal class Challenge
 {
-    public Challenge(Guid Id, Guid UserGuid, DateTime CompletedAt) { this.Id = Id; this.UserGuid = UserGuid; this.CompletedAt = CompletedAt; }
+    public Challenge(Guid Id, Guid UserGuid, DateTime CompletedAt)
+    {
+        this.Id = Id;
+        this.UserGuid = UserGuid;
+        this.CompletedAt = CompletedAt;
+    }
+
     [Column("id")]
     public Guid Id { get; set; }
+
     [Column("user_guid")]
     public Guid UserGuid { get; set; }
+
     [Column("completed_at")]
     public DateTime CompletedAt { get; set; }
+
     public ICollection<ChallengeUnit> ChallengeUnits { get; set; }
 }
